84aa1c022dc68a7da9421fa29fb42be1cf67b641
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2025-12-24 09:43:31 +0100

    new

--- app/services/page.js
+++ app/services/page.js
@@ -10,7 +10,7 @@ export default function ServicesPage() {
         {/* Abstract Background Elements */}
         <div className="absolute inset-0 opacity-20 pointer-events-none">
           <img 
-            src="https://images.unsplash.com/photo-1541888946425-d81bb19480c5?q=80&w=2070" 
+            src="https://images.unsplash.com/photo-1504307651254-35680f356dfd?q=80&w=876&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             className="w-full h-full object-cover" 
             alt="Background" 
           />
